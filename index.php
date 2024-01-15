ee744fbe0a4835d5893f5f20b10479598726e0c3
Simone Murri <simonemurri701@gmail.com>
2024-01-15 16:36:37 +0100

    inserito array hotel, usando il foreach per stamparlo in pagina

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    foreach ($hotels as $hotel) {
+        echo '<h2>' . $hotel['name'] . '</h2>';
+        echo '<p><strong>Description:</strong> ' . $hotel['description'] . '</p>';
+        echo '<p><strong>Parking:</strong> ' . ($hotel['parking'] ? 'Yes' : 'No') . '</p>';
+        echo '<p><strong>Vote:</strong> ' . $hotel['vote'] . '</p>';
+        echo '<p><strong>Distance to Center:</strong> ' . $hotel['distance_to_center'] . ' km</p>';
+    }
+
+?>
